f1cc94def21c7f0f00e4a0b9da651b9485f6a129
heck_evil <heck_evil@naver.com>
2021-08-25 16:42:35 +0900

    2021 08 24 DB연결

---
+++ src/main/java/hello/hellospring/repository/UserVo.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserVo {
+    private int uno;
+    private String unm;
+}
